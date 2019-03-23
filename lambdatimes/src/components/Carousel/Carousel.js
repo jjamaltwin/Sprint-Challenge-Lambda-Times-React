56dad5b8bb26f1fe8b68de91c536664ce8baa79c
unknown <jjamal95@gmail.com>
2019-03-23 13:52:12 -0500

    finished functionality for left and right buttons on carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,34 +1,71 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data';
+import CarousePics from './Images';
+
+
 
 
 export default class Carousel extends Component {
   constructor(props){
     super(props);
-    const images = [
-      './assets/carousel/computer.jpeg',
-      './assets/carousel/mountains.jpeg',
-      './assets/carousel/trees.jpeg',
-      './assets/carousel/turntable.jpeg'
-    ]
 
     this.state = {
       currentIndex: 0,
-      currentImage: images[0]
+      images: [],
+      currentImage: ''
     }
   }
   componentDidMount(){
-
+    this.setState(state => {
+      const curr = CarousePics[state.currentIndex];
+      return {
+        images: CarousePics,
+        currentImage: curr
+      }
+    })
   }
 
   leftClick = () => {
-
+    if(this.state.currentIndex > 0) {
+    this.setState({
+      currentIndex: this.state.currentIndex-1,
+      currentImage: this.state.images[this.state.currentIndex-1]
+    })
+    }
+    else if (this.state.currentIndex === 0) {
+      this.setState({
+        currentIndex: this.state.images.length,
+        currentImage: this.state.images[this.state.images.length-1]
+      })
+    }
+    else {
+      this.setState({
+        currentIndex: 0,
+        currentImage: this.state.images[0]
+      })
+    }
   }
 
   rightClick = () => {
+    if(this.state.currentIndex < this.state.images.length - 1) {
     this.setState({
-      currentIndex: this.state.currentIndex+1
+      currentIndex: this.state.currentIndex+1,
+      currentImage: this.state.images[this.state.currentIndex+1]
     })
+    }
+    else if (this.state.currentIndex === this.state.images.length-1) {
+      this.setState({
+        currentIndex: 0,
+        currentImage: this.state.images[0]
+      })
+    }
+    else {
+      this.setState({
+        currentIndex: 0,
+        currentImage: this.state.images[0]
+      })
+    }
+
   }
 
   selectedImage = () => {
---
+++ lambdatimes/src/components/Carousel/Images.js
@@ -0,0 +1,9 @@
+
+const CarouselPics = [
+    './assets/carousel/computer.jpeg',
+    './assets/carousel/mountains.jpeg',
+    './assets/carousel/trees.jpeg',
+    './assets/carousel/turntable.jpeg'
+  ]
+  
+  export default CarouselPics;
